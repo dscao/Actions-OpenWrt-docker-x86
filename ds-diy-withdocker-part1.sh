7a9bf851b5f0620ce88ddd9341ea6a54d1d01744
dscao <dscao.com@gmail.com>
2023-03-19 12:14:56 +0800

    Update ds-diy-withdocker-part1.sh

--- ds-diy-withdocker-part1.sh
+++ ds-diy-withdocker-part1.sh
@@ -13,7 +13,7 @@
 # Uncomment a feed source
 # Add a feed source
 sed -i "/helloworld/d" "feeds.conf.default"
-echo "src-git helloworld https://github.com/fw876/helloworld.git" >> "feeds.conf.default"
+# echo "src-git helloworld https://github.com/fw876/helloworld.git" >> "feeds.conf.default"
 echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 
 mkdir -p files/usr/share
