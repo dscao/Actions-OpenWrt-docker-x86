59414c1fc67eec617d20ca9cec8bdaa56e42c93f
dscao <dscao.com@gmail.com>
2023-04-18 09:38:44 +0800

    Update ds-diy-withdocker-part1.sh

--- ds-diy-withdocker-part1.sh
+++ ds-diy-withdocker-part1.sh
@@ -13,7 +13,7 @@
 # Uncomment a feed source
 # Add a feed source
 sed -i "/helloworld/d" "feeds.conf.default"
-# echo "src-git helloworld https://github.com/fw876/helloworld.git" >> "feeds.conf.default"
+echo "src-git helloworld https://github.com/fw876/helloworld.git" >> "feeds.conf.default"
 echo 'src-git passwallluci https://github.com/xiaorouji/openwrt-passwall;luci' >>feeds.conf.default
 echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall;packages' >>feeds.conf.default
 
